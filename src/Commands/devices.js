4ffd04235aa54b3e440a79a056903079c90d149e
ASTRO KNIGHT <joel7simon7@gmail.com>
2021-08-29 16:20:38 +0530

    src

---
+++ src/Commands/devices.js
@@ -0,0 +1,34 @@
+/** @format */
+
+const Command = require("../Structures/Command.js");
+
+const Discord = require("discord.js");
+
+module.exports = new Command({
+    name: "ping",
+    description: "Shows the ping of the bot!",
+    async run(message, client, run) {
+        let user =
+            message.mentions.members.last() ||
+            message.guild.members.cache.get(args[0]) ||
+            message.member;
+        const devices = user.presence?.clientStatus || {};
+
+        const description = () => {
+            if (devices > 1) {
+                const entries = Object.entries(devices).map(
+                    (value) => value[0]
+                );
+                return `Device: ${entries}`;
+            } else {
+                const entries = Object.entries(devices).map(
+                    (value, index) => `${index + 1}) ${value[0]}`).join("\n");
+                return `Devices: \n${entries}`
+            }
+        };
+        const embed = new Discord.MessageEmbed()
+        embed.setAuthor(user.user.tag, user.user.displayAvatarURL())
+            .setDescription(description());
+            message.channel.send({ embeds: [embed]})
+    },
+});

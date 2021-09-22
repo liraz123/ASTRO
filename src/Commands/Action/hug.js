7ee5428da3b3c9320a070597e13ae5ccf43a9a6e
ASTRO KNIGHT <joel7simon7@gmail.com>
2021-09-23 00:41:04 +0530

    src

---
+++ src/Commands/Action/hug.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+const { MessageEmbed } = require("discord.js");
+
+const Command = require("../Structures/Command.js");
+
+module.exports = new Command({
+  name: "hug",
+  description: "",
+  permission: "SEND_MESSAGES",
+  async run(message, args, client) {
+    let user = message.mentions.members.first();
+    if (!user) return message.channel.send("**Mention a member!**");
+
+    axios
+      .get("https://api.waifu.pics/sfw/hug")
+      .then((res) => {
+        const embed = new MessageEmbed()
+          .setTitle(
+            `${message.author.username} gave hug to ${user.user.username}`
+          )
+          .setImage(res.data.url)
+          .setColor("RANDOM");
+        message.channel.send({ embeds: [embed] });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+});

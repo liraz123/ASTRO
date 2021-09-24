88047e70b7483f02d2aea7168ffb0b19c813dc2f
ASTRO KNIGHT <joel7simon7@gmail.com>
2021-09-24 23:02:10 +0530

    src

--- src/Data/config.js
+++ src/Data/config.js
@@ -2,5 +2,5 @@ require('dotenv').config({path:"./src/Data/.env"});
 
 module.exports = {
     prefix: '+',
-    token: Nzk1ODYzOTU2MDQ0MzgyMjE4.X_PkEQ.hykbbHnEHpJtOgC9vFcjHw3sugo
+    token: "Nzk1ODYzOTU2MDQ0MzgyMjE4.X_PkEQ.hykbbHnEHpJtOgC9vFcjHw3sugo"
   };

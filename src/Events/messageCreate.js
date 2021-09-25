1c80dcffcf48cd46db6f6137ba2b9ad2abcc305a
ASTRO KNIGHT <joel7simon7@gmail.com>
2021-09-25 21:38:18 +0530

    src

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,35 +1,29 @@
 /** @format */
 
-const config = require("../Data/config.js");
-
 const Event = require("../Structures/Event.js");
 
 module.exports = new Event("messageCreate", (client, message) => {
-  if (message.author.bot || !message.content.startsWith(config.prefix)) return;
+	if (message.author.bot) return;
 
-  const args = message.content.slice(config.prefix.length).split(/ +/);
+	if (!message.content.startsWith(client.prefix)) return;
 
-  const cmdName = args[0];
+	const args = message.content.substring(client.prefix.length).split(/ +/);
 
-  const command =
-    client.commands.get(cmdName) ||
-    client.commands.find(
-      (command) => command.aliases && command.aliases.includes(cmdName)
-    );
+	const command = client.commands.find(cmd => cmd.name == args[0].toLowerCase()) || client.aliases.get(args[0].toLowerCase());
+	
+	if (!command) return message.reply(`${args[0]} is not a valid command!`);
 
-  if (!command) return message.reply(`${command} is not a valid command!`);
+	if (!["BOTH", "TEXT"].includes(command.type))
+		return message.reply(
+			"<a:warn_:878917634668781629> That command is only available via slash command!"
+		);
 
-  if (!["BOTH", "TEXT"].includes(command.type))
-    return message.reply(
-      "<a:warn_:878917634668781629> That command is only available via slash command!"
-    );
+	const permission = message.member.permissions.has(command.permission, true);
 
-  const permission = message.member.permissions.has(command.permission, true);
+	if (!permission)
+		return message.reply(
+			`<a:warn_:878917634668781629> You do not have the permission \`${command.permission}\` to run this command!`
+		);
 
-  if (!permission)
-    return message.reply(
-      `<a:warn_:878917634668781629> You do not have the permission \`${command.permission}\` to run this command!`
-    );
-
-  command.run(message, args, client);
+	command.run(message, args, client);
 });

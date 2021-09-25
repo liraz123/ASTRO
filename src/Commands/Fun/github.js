6ded71d83c8a200032f364dab8f519bcb876f1e1
ASTRO KNIGHT <joel7simon7@gmail.com>
2021-09-25 16:23:37 +0530

    src

--- src/Commands/Fun/github.js
+++ src/Commands/Fun/github.js
@@ -2,6 +2,8 @@ const Command = require("../../Structures/Command");
 
 const fetch = require("node-fetch");
 
+const Discord = require("discord.js");
+
 module.exports = new Command({
   name: "github",
   description: "Shows the profile of any github user",
@@ -12,7 +14,18 @@ module.exports = new Command({
     fetch(`https://luminabot.xyz/api/json/github?username=${user}`)
       .then((r) => r.json())
       .then((f) => {
-        console.log(f);
+        const embed = new Discord.MessageEmbed()
+          .setTitle(`${f.name} (Github link)[${f.url}]`)
+          .setThumbnail(f.avatar)
+          .addField(`Company`, `${f.company}`)
+          .addField(`Bio`, `${f.bio}`)
+          .addField(`Location`, `${f.location}`)
+          .addField(`Followers`, `${f.followers}`)
+          .addField(`Following`, `${f.following}`)
+          .addField(`Public repos`, `${f.public_repos}`)
+          .addField(`Public gists`, `${f.public_gists}`)
+          .setFooter(`Acc created: ${f.created_at}`);
+        message.channel.send({ embeds: [embed] });
       });
   },
 });

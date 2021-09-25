cefbbe89e7577e592e8bceb87425bca50ab97b19
ASTRO KNIGHT <joel7simon7@gmail.com>
2021-09-25 15:57:33 +0530

    src

--- src/Commands/Fun/github.js
+++ src/Commands/Fun/github.js
@@ -1,15 +1,16 @@
 const Command = require("../../Structures/Command");
 
+const fetch = require("node-fetch");
 
 module.exports = new Command({
-    name: "github",
-    description: "Shows the profile of any github user",
-    permission: "SEND_MESSAGES",
-    async run(message, args, client){
-        const user = args.slice(1).join(" ")
+  name: "github",
+  description: "Shows the profile of any github user",
+  permission: "SEND_MESSAGES",
+  async run(message, args, client) {
+    const user = args.slice(1).join(" ");
 
-        fetch(`luminabot.xyz/api/json/github?username=${user}`).then((res) => {
-            console.log(res)
-        })
-    }
-})
+    fetch(`luminabot.xyz/api/json/github?username=${user}`).then((res) => {
+      console.log(res);
+    });
+  },
+});
